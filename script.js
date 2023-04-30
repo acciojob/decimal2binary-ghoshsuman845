e7d0fd79ad3689e10fb29afb8eb81602ed8e609e
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-04-30 14:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(decimal) {
+  let binary = '';
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
 }
 
-module.exports = threeSum;
+// Example usage
+console.log(decimalToBinary(7)); // Output: 111
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(33)); // Output: 100001
