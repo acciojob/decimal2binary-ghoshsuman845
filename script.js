7cac11d0d717b45c4e6cbfba7590a4e629522536
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-05-11 17:03:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function dtb(decimal)
-	{
-		let binary='';
+function decimalToBinary(num){
+let bin = 0;
+let rem, i = 1, step = 1;
+while (x != 0) {
+rem = x % 2;
+x = parseInt(x / 2);
+bin = bin + rem * i;
+i = i * 10;
+}
+console.log(Binary: ${bin});
+}
 
-		while(decimal>0)
-			{
-				const remainder=decimal%2;
-				binary+=remainder;
-				decimal=Math.floor(decimal/2);
-			}
-		return binary.split('').reverse().join('');
-	
-	}
-module.exports=dtb();
+return parseInt(binaryStr.split("").reverse().join(""))
+
+module.exports = decimalToBinary;
