30fcfeb4ea733c821d11328776e7a76302e854f1
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-05-22 20:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function decimalToBinary(num){
-    let binaryStr = ""
+let binaryStr = ""
 
-    while(num>0){
+while(num>0){
 
-        let rem = num%2;
+let rem = num%2;
 
-        // Method 1 - Appending the new binary character to the front of the string
-        // binaryStr = rem.toString() + binaryStr
+// Method 1 - Appending the new binary character to the front of the string
+// binaryStr = rem.toString() + binaryStr
 
-        // Method 2 - Append it to the end
-        binaryStr = binaryStr + rem.toString()
+// Method 2 - Append it to the end
+binaryStr = binaryStr + rem.toString()
 
-        num = Math.floor(num/2)
-    }
-    // Method - 1
-    // return binaryStr
+num = Math.floor(num/2)
+}
+// Method - 1
+// return binaryStr
 
-
-    // Method 2- Reverse it
-    return binaryStr.split("").reverse().join("")
+// Method 2- Reverse it
+return binaryStr.split("").reverse().join("")
 }
 
-module.exports = decimalToBinary;
+module.exports = decimalToBinary
